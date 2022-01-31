e14f81cc61e4d77a584de9baddfa185e66ee1e09
Paulosmj <psmeneguesso@gmail.com>
2022-01-31 17:20:53 -0300

    Module.css

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+import styles from '../styles/Estiloso.module.css';
+
+export default function Estiloso() {
+  return (
+    <div className={styles.roxo}>
+      <h1>Estilo usando CSS Módulos</h1>
+    </div>
+  );
+}
