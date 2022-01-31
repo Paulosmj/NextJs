20236a222d81fb2ece0e5929fec978d95347f784
Paulosmj <psmeneguesso@gmail.com>
2022-01-31 16:46:13 -0300

    Primeiro componente & rotas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 export default function Inicio() {
   return 'Início';
 }
---
+++ src/pages/teste.jsx
@@ -0,0 +1,8 @@
+export default function Teste() {
+  return (
+    <div>
+      <h1>Título</h1>
+      <h2>Next.js & React</h2>
+    </div>
+  );
+}
