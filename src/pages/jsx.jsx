08b32a6bb168ae0d9d040df6ee36901093e36225
Paulosmj <psmeneguesso@gmail.com>
2022-01-31 16:58:20 -0300

    Integração JSX-JavaScript

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+export default function Jsx() {
+  const titulo = <h1>JSX é um conceito central</h1>;
+
+  function subtitulo() {
+    return <h2>{'muito legal'.toUpperCase()}</h2>;
+  }
+
+  return (
+    <div>
+      {titulo}
+      {subtitulo()}
+    </div>
+  );
+}
