b5834347e438fea47c2cfa328f3ac45d005d2209
Paulosmj <psmeneguesso@gmail.com>
2022-02-01 17:09:31 -0300

    Navegação dinâmica

---
+++ src/pages/cliente/[filial]/[Codigo].jsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import Layout from '../../../components/Layout';
+import { useRouter } from 'next/router';
+
+export default function CodigoPorCodigo() {
+  const router = useRouter();
+
+  return (
+    <Layout titulo="Navegação Dinâmica">
+      <div>Código = {router.query.codigo}</div>
+      <div>Filial = {router.query.filial}</div>
+    </Layout>
+  );
+}
